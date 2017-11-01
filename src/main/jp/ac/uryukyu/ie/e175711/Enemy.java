1b4d303bdaa595fe4c035f6e1e392c2d9380f10f
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2017-11-02 01:54:11 +0900

    last commit

--- src/main/jp/ac/uryukyu/ie/e175711/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175711/Enemy.java
@@ -5,10 +5,10 @@ public class Enemy extends LivingThing {
         super(name,maximumHP,attack);
     }
     public void wounded(int damage) {
-        hitPoint -= damage;
-        if(hitPoint < 0) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+         setHitPoint(getHitPoint()-damage);
+        if(getHitPoint() < 0) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 }
--- src/main/jp/ac/uryukyu/ie/e175711/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175711/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e175711;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
     public LivingThing(String name,int maximumHP,int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -17,6 +17,27 @@ public class LivingThing {
     }
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHitPoint() {
+        return hitPoint;
+    }
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+    public int getAttack() {
+        return attack;
+    }
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+    public boolean getDead() {
+        return dead;
+    }
+    public void setDead(boolean dead) {
+        this.dead = dead;
     }
     public void attack(LivingThing opponent) {
         if (!dead) {
