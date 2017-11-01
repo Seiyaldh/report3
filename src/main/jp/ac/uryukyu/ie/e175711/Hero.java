8afa0888dffd395dcd29c157fc76661432f1e2f4
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2017-11-02 01:10:44 +0900

    second commit

--- src/main/jp/ac/uryukyu/ie/e175711/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e175711/Hero.java
@@ -3,6 +3,12 @@ package jp.ac.uryukyu.ie.e175711;
 public class Hero extends LivingThing{
     public Hero(String name,int maximumHP,int attack) {
         super(name,maximumHP,attack) ;
-
+    }
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if(hitPoint < 0) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
     }
 }
